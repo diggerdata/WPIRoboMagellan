29b2f5a363e4345330665d43f2fefb0485149f8b
diggerdata <robotincad@gmail.com>
2018-02-12 00:46:37 -0500

    Added PID and rosserial

--- teensy/wpirm_teensy/src/main.cpp
+++ teensy/wpirm_teensy/src/main.cpp
@@ -1,9 +1,51 @@
-#include <Arduino.h>
+#include "main.h"
+
+ros::NodeHandle  nh;
+
+void motor_cb(const std_msgs::UInt16& cmd_msg) {
+}
+
+// ROS subscribers and transform publishers
+ros::Subscriber<std_msgs::UInt16> sub("motor", motor_cb);
+geometry_msgs::TransformStamped t;
+tf::TransformBroadcaster broadcaster;
+
+// PID setup
+PID pid = PID(0.1, 0.01, 0);
+
+// Odometry vars
+double x = 0.0;
+double y = 0.0;
+double theta = 0.0;
+
+char base_link[] = "/base_link";
+char odom[] = "/odom";
 
 void setup() {
-    // put your setup code here, to run once:
+    nh.initNode();
+    nh.subscribe(sub);
+    broadcaster.init(nh);
+
+    // PID set params
+    pid.setOutputLimits(-100,100);
+	pid.setOutputRampRate(10);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+    nh.spinOnce();
+    delay(1);
+}
+
+void transform_broadcaster() {
+    // tf odom->base_link
+    t.header.frame_id = odom;
+    t.child_frame_id = base_link;
+    
+    t.transform.translation.x = x;
+    t.transform.translation.y = y;
+    
+    t.transform.rotation = tf::createQuaternionFromYaw(theta);
+    t.header.stamp = nh.now();
+    
+    broadcaster.sendTransform(t);
 }
---
+++ teensy/wpirm_teensy/src/main.h
@@ -0,0 +1,12 @@
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <Arduino.h>
+#include <ros.h>
+#include <std_msgs/UInt16.h>
+#include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
+#include "PID.h"
+
+#endif
